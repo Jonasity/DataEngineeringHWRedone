9608a39937a461cbc1f99d7dab1f738fb67ee27b
Jonas <medeisisj@gmail.com>
2023-03-25 17:20:16 +0200

    First commit

---
+++ Task1.py
@@ -0,0 +1,30 @@
+from framework import map_reduce
+from typing import List, Tuple, Callable, Dict
+
+# Define the data directory containing the CSV files
+data_dir = "data/clicks"
+
+# Define the output directory where the output CSV file will be saved
+output_dir = "data/total_clicks"
+
+#Define the list of column titles for the output CSV file
+output_columns = ["date", "count"]
+
+#Define the map and reduce functions
+def map_func(row: Dict) -> List[Tuple[str, int]]:
+    """Maps the specified column in each row to a list of (value, 1) tuples"""
+    return [(row, 1)]
+
+def reduce_func(result: Dict, item: Tuple[str, int]) -> Dict:
+    """Reduces a list of (date, count) tuples into a dictionary with the counts"""
+    data, count = item
+    if data in result:
+        result[data] += count
+    else:
+        result[data] = count
+    return result
+
+# Call the map_reduce function
+result = map_reduce(data_dir, map_func, reduce_func, output_dir, output_columns)
+
+print(result)
---
+++ framework.py
@@ -0,0 +1,46 @@
+# MapReduce Framework using Higher Order Functions
+import os
+import csv
+from typing import List, Tuple, Callable, Dict
+
+def mapper(file_path: str, map_func: Callable, column_name: str) -> List:
+    #Applies the map_func to the column of each row in the CSV file
+    data = []
+    with open(file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        # Get the column names from the first row
+        column_names = next(reader)
+        # Get the index of the desired column name
+        column_index = column_names.index(column_name)
+        # Apply the map function to the column of each row
+        for row in reader:
+            data.extend(map_func(row[column_index]))
+    return data
+
+
+def reducer(data: List, reduce_func: Callable) -> Dict:
+    #Applies the reduce_func to every element in the data list
+    result = {}
+    for item in data:
+        result = reduce_func(result, item)
+    return result
+
+def map_reduce(data_dir: str, map_func: Callable, reduce_func: Callable, output_dir: str, output_columns: List[str]) -> Dict:
+    #Applies the map_func to every row in every CSV file in the data_dir directory, then applies the reduce_func to the result and outputs it to a CSV file
+    column_name=output_columns[0]
+    mapped_data = []
+    for filename in os.listdir(data_dir):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(data_dir, filename)
+            mapped_data.extend(mapper(file_path, map_func,column_name))
+    reduced_data = reducer(mapped_data, reduce_func)
+    # Write the reduced data to a CSV file
+    output_file = output_dir+".csv"
+    #output_file = os.path.join(output_dir, "output.csv").replace("\\","/")
+    with open(output_file, mode="w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=output_columns)
+        writer.writeheader()
+        for key, value in reduced_data.items():
+            writer.writerow({output_columns[0]: key, output_columns[1]: value})
+    
+    return reduced_data
